4c89c90b04978cf4208810caca010b643caac5ad
mongus <kjyanus@gmail.com>
2021-11-09 00:25:07 +0900

    initial commit

---
+++ report/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .user_data import user_data
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def reportMain(request, id):
+    data = user_data[id]
+    return render(request, "report.html", {"id": id, "data": data})
